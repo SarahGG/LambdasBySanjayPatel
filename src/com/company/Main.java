377a72fad367c6cce12d73968498e9f0faf6f12a
Sarah Guarino <guarino.sgg@gmail.com>
2019-11-07 11:25:23 -0500

    Modifying Main Class to use functional arugments.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(process("Hello, World!"));
+        System.out.println( process("Hello, World!", new Processor(){
+            public String process(String str) {
+                return str.toUpperCase();
+            }
+        }) );
     }
 
-    private static String process(String str) {
-        return str.toUpperCase();
+    private static String process(String str, Processor processor) {
+        return processor.process(str);
     }
+}
+
+interface Processor {
+    String process (String str);
 }
